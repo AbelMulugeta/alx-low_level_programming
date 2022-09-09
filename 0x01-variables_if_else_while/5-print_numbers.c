f12f44e46199a47b81e4c47e7af6d3ed71ae3175
AbelMulugeta <abeltesema6@gmail.com>
2022-09-09 12:18:13 -0700

    ommit q and e

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+  * main - prints single numbers
+  * Return: 0
+**/
+int main(void)
+{
+	int x;
+
+	for (x = 1 ; x <= 9 ; x++)
+	{
+		printf("%d", x);
+
+	}
+
+	printf("\n");
+	return (0);
+
+}
