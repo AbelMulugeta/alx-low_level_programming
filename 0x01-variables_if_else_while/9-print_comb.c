cdfdd41a8dbca8616c6fc5e3c067e394bb4bea8b
AbelMulugeta <abeltesema6@gmail.com>
2022-09-09 12:45:48 -0700

    comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+* main - comb
+* Return: always 0
+*/
+int main(void)
+{
+int n;
+
+n = 48;
+
+while (n <= 57)
+{
+putchar (n);
+if (n != 57)
+{
+putchar(',');
+putchar(' ');
+}
+n++;
+}
+putchar ('\n'); 
+return (0);
+
+}
