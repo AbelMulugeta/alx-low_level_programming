fac8b4796a61588f6cd23ba6932c4d8562eb207e
AbelMulugeta <abeltesema6@gmail.com>
2022-09-09 12:10:19 -0700

    ommit q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - ommits q  & s and prints every letter
+  * Return: 0 
+**/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		if (ch != 'q' && ch != 'e')
+		putchar(c);
+
+	}	
+
+	putchar('\n');
+	return (0);
+
+}
