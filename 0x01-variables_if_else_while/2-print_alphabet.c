969212b365c22bacd2126efff7fa3b825f44f3fc
AbelMulugeta <abeltesema6@gmail.com>
2022-09-09 09:34:25 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - prints a-z
+  * Return: 0 on success
+**/
+int main(void)
+{
+	for (char ch = 'a', ch <= 'z', ch++)
+	{
+		putchar(ch);
+	}
+	putchar(\n);
+	return (0);
+
+
+
+}
