efcfbc59d54a084f96d7f9d7c7f1b615be836424
AbelMulugeta <abeltesema6@gmail.com>
2022-09-09 13:07:34 -0700

    all

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,25 +9,25 @@ int x = 0;
 int y;
 int z;
 
-while (x <= 7)
+while (x <= '7')
 {
 y = x + 1;
-while (y <= 8)
+while (y <= '8')
 {
 z = y + 1;
-while (z <= 9)
+while (z <= '9')
 {
 putchar(x);
 putchar(y);
 putchar(z);
-if (x == 7 && y == 8 && z == 9)
+if (x == '7' && y == '8' && z == '9')
 {
 putchar('\n');
 }
 else
 {
-putchar(,);
-putchar( );
+putchar(',');
+putchar(' ');
 }
 z++;
 }
