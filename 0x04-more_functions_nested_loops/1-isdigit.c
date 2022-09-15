ff7fdeb7385c873cdce7523216884ca0f571d432
AbelMulugeta <abeltesema6@gmail.com>
2022-09-15 14:42:22 -0700

    nested

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
 * _isdigit - Returns 1 for a digit
 * @c: takes a character
 * Return: 0 for uppercase, 1 for lowercase
 **/
-
 int _isdigit(int c)
 {
 if (c >= 0 && c <= 9)
